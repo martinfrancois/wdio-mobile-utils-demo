aca6af40b36acc184a7024f5052e01d591f7fb1a
martinfrancois <f.martin@fastmail.com>
2020-05-09 14:05:54 +0200

    fix app id getter

--- e2e/utils/utils.ts
+++ e2e/utils/utils.ts
@@ -4,7 +4,5 @@ const packagejson = require('../../package.json');
  * Returns the packageId for Android and the bundleId for iOS.
  */
 export function getAppId() {
-    return browser.isIOS
-        ? packagejson.nativescript.id.ios
-        : packagejson.nativescript.id.android;
+    return packagejson.nativescript.id;
 }
